2864f0a405393f35fd18d25e12eaaa08d2e6e1fd
AMRBY <bouyoussefamr@gmail.com>
2024-10-03 21:31:56 +0000

    mandatory

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,10 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const user = createUser();
   const photo = uploadPhoto();
-  return Promise.all([photo, user])
+  return Promise
+    .all([photo, user])
     .then(([p, u]) => {
-      console.log(p.body, u.firstName, u.lastName);
+      console.log(`${p.body} ${u.firstName} ${u.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
