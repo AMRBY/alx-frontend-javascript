7f778a5f5498eb1c95b953fb4aac17aea5e92ef8
AMRBY <bouyoussefamr@gmail.com>
2024-10-03 13:01:26 +0000

    mandatory

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error(''));
-    }
-  }).finally(() => (console.log('Got a response from the API')));
+export default function getFullResponseFromAPI(promise) {
+  return promise.then(() => ({
+    status: 200, body: 'success',
+  })).catch((error) => {
+    error();
+  }).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
