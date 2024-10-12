d1b958df28cf167f341b635b271763e4faac0d7a
AMRBY <bouyoussefamr@gmail.com>
2024-10-12 16:54:32 +0000

    mandatory

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   const len = startString.length;
   let values = '';
-  if (len !== 0) {
+  if (len !== 0 || typeof set !== 'object' || typeof startString !== 'string') {
     for (const value of set) {
       if (startString === value.slice(0, len)) {
         values += value.slice(len, value.length).concat('-');
       }
     }
+    return values.slice(0, values.length - 1);
   }
-  return values.slice(0, values.length - 1);
+  return values;
 }
