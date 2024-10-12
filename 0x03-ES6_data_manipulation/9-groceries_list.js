5d8686e6cc71b5412aba88360a304df04d9fab16
AMRBY <bouyoussefamr@gmail.com>
2024-10-12 14:37:01 +0000

    mandatory

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,14 +1,3 @@
-
-const groceriesList = () => {
-  const fruits = new Map([
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ]);
-
-  return fruits;
-};
-
-export default groceriesList;
+export default function groceriesList() {
+  return new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+}
