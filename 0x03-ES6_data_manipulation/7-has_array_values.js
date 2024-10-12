e96f4271c5456bb4cbede46df4bd997610f0f601
AMRBY <bouyoussefamr@gmail.com>
2024-10-12 13:38:47 +0000

    mandatory

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,12 +1,8 @@
-
-const hasValuesFromArray = (set, array) => {
-  for (const item of array) {
-    if (!set.has(item)) {
+export default function hasValuesFromArray(set, array) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (!set.has(array[i])) {
       return false;
     }
   }
-
   return true;
-};
-
-export default hasValuesFromArray;
+}
